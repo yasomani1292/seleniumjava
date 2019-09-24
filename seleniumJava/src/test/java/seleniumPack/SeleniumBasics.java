159db3ea1f26860766b7aeac031a58bc267c740f
Manikandan P <mani.alagarece@gmail.com>
2019-09-24 21:36:02 +0530

    Driver config and loading application

--- seleniumJava/src/test/java/seleniumPack/SeleniumBasics.java
+++ seleniumJava/src/test/java/seleniumPack/SeleniumBasics.java
@@ -1,8 +1,16 @@
 package seleniumPack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumBasics {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com/samples.php");
+		
 		
 		
 		
