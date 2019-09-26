30114a8f78bd69c09e740984022c7c1b8f9f1716
Manikandan P <mani.alagarece@gmail.com>
2019-09-26 07:29:32 +0530

    Submiting form - mycontactform

--- seleniumJava/src/test/java/seleniumPack/SeleniumBasics.java
+++ seleniumJava/src/test/java/seleniumPack/SeleniumBasics.java
@@ -17,45 +17,33 @@ public class SeleniumBasics {
 		// locators 8
 		// id, name, linktext, partialLinkText, tagname, classname, xpath, cssSelector
 
+		// Checkbox
 		WebElement chk1 = driver.findElement(By.name("email_to[]"));
 		chk1.click();
 		System.out.println(chk1.isSelected());
-
 		driver.findElement(By.xpath("//input[@name='email_to[]' and @value='1']")).click();
-	
-		//Select 3rd checkbox
 		driver.findElement(By.xpath("//input[@name='email_to[]' and @value='2']")).click();
-		
+
+		// Text box
 		driver.findElement(By.id("subject")).sendKeys("Selenium");
-		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");    
-	    driver.findElement(By.id("q1")).sendKeys("java"); 
-	    
-	    
-	    
-	    Select dd1 = new Select(driver.findElement(By.id("q3")));
-	    dd1.selectByIndex(1);
-	    dd1.selectByVisibleText("Third Option");
-	    dd1.selectByValue("Fourth Option");
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
+		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("q1")).sendKeys("java");
+
+		// Dropdown
+		Select dd1 = new Select(driver.findElement(By.id("q3")));
+		dd1.selectByIndex(1);
+		dd1.selectByVisibleText("Third Option");
+		dd1.selectByValue("Fourth Option");
+
+		// Radio button
+		driver.findElement(By.xpath("//input[@value='Third Option']")).click();
+
+		// Calendar
+		driver.findElement(By.name("q7")).click();
+		driver.findElement(By.xpath("//a[@class='ui-state-default' and text()='17']")).click();
+		
+		driver.findElement(By.name("visver_code")).sendKeys("ABCD");
+		driver.findElement(By.className("submit")).click();
 
 	}
 
