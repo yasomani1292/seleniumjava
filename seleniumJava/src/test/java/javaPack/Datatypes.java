f7eb9e8a8ff12367d76bd0ec1232d1ee9899e15c
Manikandan <55494815+mani0588@users.noreply.github.com>
2019-11-13 07:52:38 +0530

    Create Datatypes.java

---
+++ seleniumJava/src/test/java/javaPack/Datatypes.java
@@ -0,0 +1,22 @@
+package javaPack;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+
+		// Datatypes - int, float, char, String and boolean
+		int num = 450;
+		float price = 450.75f;
+		char c = 'C';
+		String anything = "alpha numeric-12345 special-!@#$%^&*() and more";
+		boolean result = true; // false
+
+		System.out.println(num);
+		System.out.println(price);
+		System.out.println(c);
+		System.out.println(anything);
+		System.out.println(result);
+
+	}
+
+}
