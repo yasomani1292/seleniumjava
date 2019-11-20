faa31c99f702591726e99798e5d59030e77f0302
Manikandan <55494815+mani0588@users.noreply.github.com>
2019-11-20 08:15:04 +0530

    Create Login.java

---
+++ seleniumJava/src/test/java/javaPack/Login.java
@@ -0,0 +1,22 @@
+package javaPack;
+
+public class Login {
+
+	public Login(String un, String pwd) {
+		// code to validate cred
+		// if not valid throw some exception
+	}
+
+	public void homePage() {
+
+	}
+
+	public void acctPage() {
+
+	}
+
+	public void historyPage() {
+
+	}
+
+}
